71440f1b7d01c177b9ca5ffcc9eccca7f3612dff
Aaditya Mall <aadityarmall@gmail.com>
2023-12-27 14:29:42 +0530

    backend finished

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const app =  express();
-const errorMiddleware = require("./middleware/error")
-const cookieParser =  require("cookie-parser")
+const app = express();
+const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
 
 //Importing Routes
-const product =  require("./routes/productsRoute");
-const user =  require('./routes/userRoute')
+const product = require("./routes/productsRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
-app.use("/api/v1", product)
-app.use("/api/v1", user)
+app.use("/api/v1", product);
+app.use("/api/v1", user);
+app.use("/api/v1", order);
 
 //Middleware for error
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 module.exports = app;
---
+++ backend/controllers/orderController.js
@@ -0,0 +1,127 @@
+const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorHandler");
+const asyncError = require("../middleware/asyncError");
+
+//Create new order
+exports.newOrder = asyncError(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user.id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+//Get single Order
+exports.getSingleOrder = asyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found", 404));
+  }
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+//get logged in user orders
+exports.myOrders = asyncError(async (req, res, next) => {
+  const order = await Order.find({ user: req.user.id });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+//get all orders --admin
+exports.getAllOrders = asyncError(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(201).json({
+    success: true,
+    orders,
+    totalAmount,
+  });
+});
+
+//Update Order Status --admin
+exports.updateOrder = asyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found", 404));
+  }
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already Dilevered this Order", 404));
+  }
+
+  order.orderItems.forEach(async (o) => {
+    await updateStock(o.product, o.quantity);
+  });
+
+  if (req.body.status === "Delivered") {
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+  product.stock = product.stock - quantity;
+  await product.save({ validateBeforeSave: false });
+}
+
+//Delete order -- admin only
+exports.deleteOrder = asyncError(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found", 404));
+  }
+
+  await order.deleteOne();
+
+  res.status(201).json({
+    success: true,
+  });
+});
