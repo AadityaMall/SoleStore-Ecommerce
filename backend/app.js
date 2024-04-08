232af797e7f3a26b6bab569fecb2157093f8cc78
Aaditya Mall <aadityarmall@gmail.com>
2024-04-08 15:37:26 +0530

    made changes

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
-app.use(express.json({limit:'50mb'}));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+const path = require("path");
+
 
 //Importing Routes
 const product = require("./routes/productsRoute");
@@ -19,6 +21,10 @@ app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 
+app.use(express.static(path.join(__dirname, "../solestore/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../solestore/build/index.html"));
+});
 //Middleware for error
 app.use(errorMiddleware);
 
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const dotenv = require('dotenv');
 const connectDatabase = require('./config/database');
 const cloudinary = require("cloudinary")
 
-// config 
+
 dotenv.config({path:"backend/config/config.env"});
+    
 
 // Connecting to Database ==>
 
