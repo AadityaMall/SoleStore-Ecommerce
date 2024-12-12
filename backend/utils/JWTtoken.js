14baab0e6854a840049658f6b83553518019f8ec
IET Mpsteme <aadityarmall@gmail.com>
2024-12-12 19:56:44 +0530

    advancements

--- backend/utils/JWTtoken.js
+++ backend/utils/JWTtoken.js
@@ -4,14 +4,14 @@ const sendToken = (user,statusCode,res,options={}) => {
     const token =  user.getJWTToken();
 
     //Options for cookie -->
-    const options = {
+    const cookieOptions = {
         expires: new Date(Date.now() + (options.expires || process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: options.secure || false, // Default to false in development
         sameSite: options.sameSite || 'Lax' // Default to 'Lax', can be overriddenx`
     }
 
-    res.status(statusCode).cookie('token',token,options).json({
+    res.status(statusCode).cookie('token', token, cookieOptions).json({
         success:true,
         user,
         token
