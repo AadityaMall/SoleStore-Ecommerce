18438d38941b03db73453151b49ac1c9dc1390d5
IET Mpsteme <aadityarmall@gmail.com>
2024-12-12 16:48:44 +0530

    vercel host

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const app = require("./app.js");
 const dotenv = require('dotenv');
-const connectDatabase = require('./config/database');
+const connectDatabase = require('./config/database.js');
 const cloudinary = require("cloudinary")
 
 
