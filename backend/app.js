14c644b5bad4b056d54b0d5dba57f5e8dcfce9f8
IET Mpsteme <aadityarmall@gmail.com>
2024-12-12 17:28:23 +0530

    advancements

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,26 @@ const app = express();
 const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-app.use(cors())
+const cors = require("cors");
+
+const allowedOrigin = "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || origin === allowedOrigin) {
+        callback(null, origin); // Allow the origin
+      } else {
+        callback(new Error("Not allowed by CORS")); // Block the origin
+      }
+    },
+    credentials: true, // Allow credentials (cookies)
+  })
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 
 //Importing Routes
 const product = require("./routes/productsRoute");
