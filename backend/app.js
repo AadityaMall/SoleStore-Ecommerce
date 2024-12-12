ced98a3a90c9ca813a521b15966e568dc1ba4b3b
IET Mpsteme <aadityarmall@gmail.com>
2024-12-12 16:43:14 +0530

    env update

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-const path = require("path");
 
 
 //Importing Routes
@@ -18,11 +17,6 @@ const order = require("./routes/orderRoute");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
-
-app.use(express.static(path.join(__dirname, "../solestore/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../solestore/build/index.html"));
-});
 
 //Middleware for error
 app.use(errorMiddleware);
