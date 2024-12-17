2bcefb0b08255da238ba30a3a0501144c5b9aafe
IET Mpsteme <aadityarmall@gmail.com>
2024-12-17 13:22:06 +0530

    cors issue

--- backend/utils/JWTtoken.js
+++ backend/utils/JWTtoken.js
@@ -8,7 +8,7 @@ const sendToken = (user,statusCode,res,options={secure:true}) => {
         expires: new Date(Date.now() + (options.expires || process.env.COOKIE_EXPIRE) * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: options.secure || false, // Default to false in development
-        sameSite: options.sameSite || 'Lax' // Default to 'Lax', can be overriddenx`
+        sameSite: 'None'
     }
 
     res.status(statusCode).cookie('token', token, cookieOptions).json({
