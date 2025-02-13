aa0b1519355e5e3879b21399bb79655e77e1d49f
IET Mpsteme <aadityarmall@gmail.com>
2025-02-13 23:31:27 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require('cors');
 
 app.use(cors({
-  origin: ['*'],
+  origin: '*',
   credentials: true // Allow cookies in requests
 }));
 
